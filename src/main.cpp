c9efa864a45d7521dc6b238963debba0095462de
CppPhil <CppPhil@users.noreply.github.com>
2021-04-13 21:09:05 +0200

    Make it work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-  std::cout << "Hello C++\n";
+  sf::RenderWindow window{
+    sf::VideoMode{800, 600},
+    "My window"
+  };
+  window.setVerticalSyncEnabled(true);
+  window.setFramerateLimit(60);
+
+  // Run the program as long as the window is open.
+  while (window.isOpen())
+  {
+    // Check all the window's events that were
+    // triggered since the last iteration of
+    // the loop.
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    sf::CircleShape shape{50.f};
+    shape.setFillColor(sf::Color{150, 50, 250});
+
+    // Set a 10-pixel wide orange outline
+    shape.setOutlineThickness(10.f);
+    shape.setOutlineColor(sf::Color{250, 150, 100});
+
+    window.draw(shape);
+
+    // End the current frame
+    window.display();
+  }
 }
